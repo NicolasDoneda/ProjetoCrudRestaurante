83b1010e58c36921316eb327fea49a4c0151f145
NicolasDoneda <nicolasdoneda231@gmail.com>
2025-06-12 14:55:30 -0300

    banco de dados novo

--- src/database/bd_restaurante.sql
+++ src/database/bd_restaurante.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 09/06/2025 às 19:27
+-- Tempo de geração: 12/06/2025 às 19:53
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -24,81 +24,149 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `pratos`
+-- Estrutura para tabela `categoria`
 --
 
-CREATE TABLE `pratos` (
+CREATE TABLE `categoria` (
   `id` int(11) NOT NULL,
-  `nome` varchar(255) NOT NULL,
-  `descricao` text NOT NULL,
-  `preco` decimal(10,2) NOT NULL,
-  `imagem` varchar(255) DEFAULT NULL
+  `nome` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Despejando dados para a tabela `pratos`
+-- Despejando dados para a tabela `categoria`
 --
 
-INSERT INTO `pratos` (`id`, `nome`, `descricao`, `preco`, `imagem`) VALUES
-(6, 'bebe', 'bebe do bem', 4555.00, '6841cd8ce3f7d_imagem.png'),
-(7, 'Macarrao Gostoso', 'Massa al dente envolvida em um molho pesto fresco feito com manjericão, pinhões e azeite extra virgem. Finalizado com queijo parmesão ralado na hora para um toque cremoso e saboroso.', 20.00, '6844f0c4cb657_comida.webp'),
-(8, 'Comida gostosa', 'Peito de frango suculento grelhado na brasa, acompanhado de legumes frescos salteados no alho e azeite, temperados com ervas finas para um sabor leve e nutritivo.', 50.00, '6844f3a97cb52_comidagostosa.webp'),
-(9, 'Nicolas', 'Massa al dente envolvida em um molho pesto fresco feito com manjericão, pinhões e azeite extra virgem. Finalizado com queijo parmesão ralado na hora para um toque cremoso e saboroso.', 50.00, '6844f3f88647f_comidagostosa.webp'),
-(10, 'Nicolas Doneda', 'Massa al dente envolvida em um molho pesto fresco feito com manjericão, pinhões e azeite extra virgem. Finalizado com queijo parmesão ralado na hora para um toque cremoso e saboroso.', 50.00, '6844f40266748_comidagostosa.webp');
+INSERT INTO `categoria` (`id`, `nome`) VALUES
+(1, 'Porção'),
+(2, 'Sobremesa'),
+(3, 'Bebida'),
+(4, 'Prato Principal');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `usuarios`
+-- Estrutura para tabela `pedido`
 --
 
-CREATE TABLE `usuarios` (
+CREATE TABLE `pedido` (
+  `id` int(11) NOT NULL,
+  `id_usuario` int(11) DEFAULT NULL,
+  `data` datetime DEFAULT current_timestamp(),
+  `status` varchar(50) DEFAULT NULL,
+  `total` decimal(8,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `prato`
+--
+
+CREATE TABLE `prato` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) DEFAULT NULL,
+  `descricao` text DEFAULT NULL,
+  `preco` decimal(6,2) DEFAULT NULL,
+  `id_categoria` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
   `id` int(11) NOT NULL,
   `nome` varchar(100) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
   `senha` varchar(255) DEFAULT NULL,
-  `tipo` enum('cliente','admin') DEFAULT NULL
+  `tipo` enum('admin','cliente') DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Despejando dados para a tabela `usuarios`
+-- Despejando dados para a tabela `usuario`
 --
 
-INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `tipo`) VALUES
-(7, 'Nicolas', 'nicolasdoneda231@gmail.com', 'Nagibe123', 'admin');
+INSERT INTO `usuario` (`id`, `nome`, `email`, `senha`, `tipo`) VALUES
+(1, NULL, NULL, '$2y$10$ra/Yhp2TPi0TNRIh5zBCw.kbGMND3.E9xwE/ZrJI4xUNKwRhP5l5G', NULL),
+(2, NULL, NULL, '$2y$10$48reyGSyhPJ8e0o7.Kcf2uW9Ao0fd6cLKgdcptOljArsFR/KVCR76', NULL),
+(3, '', '', '$2y$10$7kRuf0njmjULz54o5VErWeimu72Qwn92KaHLfKaOh/cwMeQ1XdXRK', ''),
+(4, '', '', '$2y$10$zDt.nwMZ.bD2Y525OoCoSemA2I9ykyqye2mwV0bEmYDSFKE6rzt2.', '');
 
 --
 -- Índices para tabelas despejadas
 --
 
 --
--- Índices de tabela `pratos`
+-- Índices de tabela `categoria`
 --
-ALTER TABLE `pratos`
+ALTER TABLE `categoria`
   ADD PRIMARY KEY (`id`);
 
 --
--- Índices de tabela `usuarios`
+-- Índices de tabela `pedido`
 --
-ALTER TABLE `usuarios`
+ALTER TABLE `pedido`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Índices de tabela `prato`
+--
+ALTER TABLE `prato`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_categoria` (`id_categoria`);
+
+--
+-- Índices de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT para tabelas despejadas
 --
 
 --
--- AUTO_INCREMENT de tabela `pratos`
+-- AUTO_INCREMENT de tabela `categoria`
 --
-ALTER TABLE `pratos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+ALTER TABLE `categoria`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT de tabela `usuarios`
+-- AUTO_INCREMENT de tabela `pedido`
 --
-ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+ALTER TABLE `pedido`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `prato`
+--
+ALTER TABLE `prato`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `pedido`
+--
+ALTER TABLE `pedido`
+  ADD CONSTRAINT `pedido_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`);
+
+--
+-- Restrições para tabelas `prato`
+--
+ALTER TABLE `prato`
+  ADD CONSTRAINT `prato_ibfk_1` FOREIGN KEY (`id_categoria`) REFERENCES `categoria` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
