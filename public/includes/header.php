65895f1eb46d135bcfbfcee152d24585ddea0959
Gabriel Souza Lima <gabriel.lima607@etec.sp.gov.br>
2025-06-15 17:18:41 -0300

    Adicionar header, footer e headerFooterStyle ao Main

--- public/includes/header.php
+++ public/includes/header.php
@@ -1,23 +1,39 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Header</title>
-    <link rel = "stylesheet" href = "../assets/css/stylesGlobal.css">
+    <title>Al Dente Zaatar | Restaurante</title>
+    <link rel="stylesheet" href="../assets/css/headerFooterStyle.css">
 </head>
-<header>
-    <nav>
-    <img src = "/ProjetoCrudRestaurante/public/assets/images/images-header/logo-marca-al-dente.png" 
-    id = "logoMarcaRestaurante" alt = "Logo Marca do Restaurante">
-        <ul>
-            <li><a href="/ProjetoCrudRestaurante/public/index.php">Home</a></li>
-            <li><a href="/ProjetoCrudRestaurante/public/layouts/cardapio-screen.php">Cardápio</a></li>
-            <li><a href="/ProjetoCrudRestaurante/public/layouts/pedidos-screen.php">Pedidos</a></li>
-            <li><a href= "/ProjetoCrudRestaurante/public/layouts/sobre-nos-screen.php">Sobre Nós</a></li>
-        </ul>
-    </nav>
-</header>
-</body>
 
+<body>
+
+    <header>
+        <nav>
+            <img 
+                src="/ProjetoCrudRestaurante/public/assets/images/images-header/logo-marca-al-dente.png"
+                class="header__logo"
+                alt="Logotipo do Restaurante Al Dente Zaatar"
+                title="Al Dente Zaatar - Início"
+            >
+            <ul>
+                <li><a href="/ProjetoCrudRestaurante/public/index.php">Home</a></li>
+                <li><a href="/ProjetoCrudRestaurante/public/layouts/cardapio-screen.php">Cardápio</a></li>
+                <li><a href="/ProjetoCrudRestaurante/public/layouts/pedidos-screen.php">Pedidos</a></li>
+                <li><a href="/ProjetoCrudRestaurante/public/layouts/sobre-nos-screen.php">Sobre Nós</a></li>
+            </ul>
+        </nav>
+        
+        <a href="/ProjetoCrudRestaurante/public/layouts/login-screen.php" title="Acessar área do usuário">
+            <img 
+                src="/ProjetoCrudRestaurante/public/assets/images/images-header/logo-usuario.png"
+                alt="Ícone representando o login ou cadastro do usuário"
+                class="user-icon"
+            >
+        </a>
+    </header>
+
+</body>
 </html>
