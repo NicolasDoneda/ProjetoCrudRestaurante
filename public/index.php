cf50f21cdaab5c34c69b5aae37ef515e2c6fb643
Gabriel Souza Lima <gabriel.lima607@etec.sp.gov.br>
2025-06-15 17:23:03 -0300

    Atualizações na home/index.php

--- public/index.php
+++ public/index.php
@@ -1,22 +1,36 @@
-<?php include '../public/includes/header.php'; ?>
 <!DOCTYPE html>
 <html lang="pt-br">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-    <link rel="stylesheet" href="./assets/css/stylesGlobal.css">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Home - Al Dente & Za’atar</title>
+    <link rel="stylesheet" href="./assets/css/homeScreenStyle.css" />
+    <link rel="stylesheet" href="./assets/css/headerFooterStyle.css" />
 </head>
 
 <body>
+    <header>
+        <?php include '../public/includes/header.php'; ?>
+    </header>
+
     <main>
-        <section id="bannerPrincipal">
-            <h2>Bem-Vindo ao <h2>
-            <h1>ALDENTE & ZA’ATAR<h1>
+        <section class="banner-principal">
+            <h1 class="titulo-secundario-home">Seja Bem-Vindo ao</h1>
+            <h2 class="titulo-primario-home">ALDENTE & ZA’ATAR</h2>
+            <p>
+                AlDente & Za’atar iniciou-se no século XX, quando Antoni Zighher apreciava os pratos da cultura italiana desde a sua infância, e trabalhou em um restaurante árabe na Bretanha (norte da França), conhecendo as riquezas de ambas as culturas. Antes de seu falecimento, Antoni resolveu misturar os incrementos gastronômicos da Itália e a picância do Líbano em um único sabor, transformando a sua curiosidade em uma experiência aconchegante e acolhedora. A abertura do restaurante ocorreu pouco depois dessa descoberta, fazendo então com que Zighher fundasse a nossa empresa, colidindo com as crenças italianas e as ideologias árabes.
+            </p>
+        </section>
+
+        <section class="secao-mais-pedidos">
+            <h2 class="titulo-principal-home">Os mais pedidos!</h2>
         </section>
     </main>
-    <?php include '../public/includes/footer.php'; ?>
+
+    <footer>
+        <?php include '../public/includes/footer.php'; ?>
+    </footer>
 </body>
 
 </html>
